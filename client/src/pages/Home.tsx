e20b24f75afc1d31357f83badbe87a6adbb3d30e
Sakib Ahmed <sakibahmedsadhin@gmail.com>
2026-02-28 04:56:35 +0600

    text change

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -20,8 +20,8 @@ export default function Home() {
 
           {/* Slightly Reduced Title Size */}
           <h1 className="text-5xl md:text-7xl font-serif font-black text-white text-shadow-md mb-8 leading-[1.1] tracking-tight">
-            Master the Art of <br />
-            <span className="text-primary text-4xl md:text-6xl">Professional Diploma In <br /> Cybersecurity</span>
+            Professional Diploma In <br />
+            <span className="text-primary">Cyber Security</span>
           </h1>
 
           <p className="text-lg md:text-xl text-gray-200 max-w-2xl mx-auto mb-12 text-shadow-sm font-light">
